bcd89d35995a67f956907e02a9cd0a5ba35b0137
xu-hongyi <13103682353@163.com>
2020-04-21 17:04:07 +0800

    连接数据库

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -4,9 +4,23 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    const { ctx } = this;
+    const { ctx, app } = this;
+    
     ctx.body = 'hello, api接口';
   }
+
+  async getArticleList(){
+    let sql = 'SELECT artical.id as id, ' +
+              'artical.title as title, ' + 
+              'artical.introduce as introduce, ' + 
+              'artical.addTime as addTime, ' + 
+              'artical.view_count as view_count, ' +
+              'type.typename as typeName ' +
+              'FROM artical LEFT JOIN type ON artical.type_id = type_id' ;
+    const results = await this.app.mysql.query(sql);
+    console.log(results)
+    this.ctx.body = {data:results}
+  } 
 }
 
 module.exports = HomeController;
--- config/config.default.js
+++ config/config.default.js
@@ -22,7 +22,26 @@ module.exports = appInfo => {
   const userConfig = {
     // myAppName: 'egg',
   };
-
+  config.mysql = {
+    // database configuration
+    client: {
+      // host
+      host: 'localhost',
+      // port
+      port: '3306',
+      // username
+    
+      user: 'root',
+      // password
+      password: 'Xu123456',
+      // database
+      database: 'react_blog',    
+    },
+    // load into app, default is open
+    app: true,
+    // load into agent, default is close
+    agent: false,
+  };
   return {
     ...config,
     ...userConfig,
